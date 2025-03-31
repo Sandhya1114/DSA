7474309b93710413877f481b0eccf171c40879ec
Sandhya1114 <146408600+Sandhya1114@users.noreply.github.com>
2025-03-31 16:52:29 +0530

    Add files via upload

---
+++ DataType_02.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int a=5;// integer --4 byte-->32bit
+    cout<< a<< endl;
+    char ch='c';//character --1byte-->8bit
+    cout<<ch<<endl;
+    bool bl = true;//boolen --1bit
+    cout<<bl<<endl;
+    float f=1.2;//floating number -->4byte
+    cout<< f<< endl;
+    double d=1.23;//double -->8byte
+    cout<< d<< endl;
+    int size=sizeof(f);
+    cout<<"size of a is:"<<size<<endl;*/
+
+    ///type casting
+    // int a='a';
+    // cout<<a<<endl;
+   //small can change in to big data conversion
+
+   //operator
+   //arithmatic +,-,%,/,*
+   /* int/int-->int
+      float/int-->float
+      double/int-->double*/
+
+    //relational operator =,>,<,>=,<=,!=
+    // int a=2;
+    // int b=4;
+    // bool first=(a!=b);
+    // cout<<first<<endl;
+    //logical operator && ,|| ,!
+    int a=0;
+    cout<<!a<<endl;
+
+}
